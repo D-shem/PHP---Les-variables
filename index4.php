bedf4fd6d49d5dda5274695895537d935d505720
D-shem <longuet.go@hotmail.fr>
2016-11-17 14:29:15 +0100

    Add files via upload

---
+++ index4.php
@@ -0,0 +1,19 @@
+<?php
+
+$bar = true;
+echo $bar;
+echo "<br>";
+
+$nombre = 5000;
+echo $nombre;
+echo "<br>";
+
+$chiffre = 5.5;
+echo $chiffre;
+echo "<br>";
+
+$hello = "bonjour le monde";
+echo $hello;
+
+
+?>

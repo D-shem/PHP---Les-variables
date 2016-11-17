45afabbd2130d565d49dadcc0d26cb23ae1b6374
D-shem <longuet.go@hotmail.fr>
2016-11-17 13:30:30 +0100

    Add files via upload

---
+++ index1.php
@@ -0,0 +1,27 @@
+<?php 
+
+echo 'Votre adresse IP est : '.$_SERVER['SERVER_SIGNATURE'];
+
+echo 'Votre adresse IP est : '.$_SERVER['DOCUMENT_ROOT'];
+
+echo 'Votre adresse IP est : '.$_SERVER['HTTP_HOST'];
+
+echo 'Votre adresse IP est : '.$_SERVER['HTTP_USER_AGENT'];
+
+echo 'Votre adresse IP est : '.$_SERVER['PATH_INFO'];
+
+echo 'Votre adresse IP est : '.$_SERVER['PATH_TRANSLATED'];
+
+echo 'Votre adresse IP est : '.$_SERVER['REQUEST_URI'];
+
+echo 'Votre adresse IP est : '.$_SERVER['REMOTE_ADDR'];
+
+echo 'Votre adresse IP est : '.$_SERVER['REMOTE_PORT'];
+
+echo 'Votre adresse IP est : '.$_SERVER['REQUEST_METHOD'];
+
+echo 'Votre adresse IP est : '.$_SERVER['SERVER_ADMIN'];
+
+
+ ?>
+
